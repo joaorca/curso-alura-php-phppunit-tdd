9590b21a52c06c07a9f974137656d7347c988bd2
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-16 20:39:04 -0300

    Testes automatizados

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,6 +7,8 @@ use Alura\Leilao\Service\Avaliador;
 
 require_once 'vendor/autoload.php';
 
+// Arrumo a casa para o test
+// Arrange - Given
 $leilao = new Leilao('Fiat 147 0Km');
 
 $maria = new Usuario('Maria');
@@ -16,7 +18,19 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+// Executo código a ser testado
+// Act - When
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
-echo $maiorValor;
+
+// Verifico se a saída é a esperada
+// Assert - Then
+$valoEsperado = 2500.0;
+
+if ($valoEsperado === $maiorValor){
+    echo "TESTE OK";
+}else {
+    echo "TESTE FALHOU";
+}
