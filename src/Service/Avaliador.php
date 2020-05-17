c2f0703b1331d03e44a9f03b5946173fa5bedb98
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-16 22:41:25 -0300

    Maiores lances

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,23 +2,34 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
     private float $maiorValor = -INF;
     private float $menorValor = +INF;
+    private array $maioresLances;
 
     public function avalia(Leilao $leilao): void
     {
-        foreach ($leilao->getLances() as $lance){
-            if ($lance->getValor() > $this->maiorValor){
+        foreach ($leilao->getLances() as $lance) {
+            if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
             }
-            if ($lance->getValor() < $this->menorValor){
+            if ($lance->getValor() < $this->menorValor) {
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances();
+        usort(
+            $lances,
+            function (Lance $lance1, Lance $lance2) {
+                return $lance1->getValor() - $lance2->getValor();
+            }
+        );
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -29,5 +40,10 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
